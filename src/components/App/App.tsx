7cedd095fe8b3e3a81a5315f17c8a5f1f6135bfd
Viktoria Zharina <159477668+ViktoriaZharina@users.noreply.github.com>
2025-05-17 15:19:09 +0300

    dfrr

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+
+function handleVote(type) {}
+
+function resetVotes() {}
 
 export default function App() {
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions />
     </div>
   );
 }
